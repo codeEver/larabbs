f0e3bf79d92c5512bd0947573393fb2394892e1f
codeEver <241998184@qq.com>
2019-11-04 12:02:00 +0000

    翻译url 标示

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,9 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 //用户资源路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 //帖子上传图片
