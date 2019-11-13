067e32e7f400d648901f59912222349c8d49b22b
codeEver <241998184@qq.com>
2019-11-13 09:09:31 +0000

    内容管理后台

--- config/administrator.php
+++ config/administrator.php
@@ -60,6 +60,7 @@ return array(
      */
     'menu' => [
                 '用户与权限' => ['users','roles','permissions',],
+                '内容管理' => ['categories','topics','replies',],
 
             ],
 
